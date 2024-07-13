27ab9ee18d7c441d73dddfe7df2cb0d5879b1085
Danhinou-Medium-Africain <louisefabre94@gmail.com>
2024-07-13 21:14:11 +0100

    Add files via upload

---
+++ traitement.php
@@ -0,0 +1,20 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $destinataire = 'cedrictognifode@gmail.com'; // Remplacez par votre adresse e-mail
+    $nom = $_POST['nom'];
+    $prenom = $_POST['prenom'];
+    $email = $_POST['email'];
+    $message = $_POST['message1'];
+
+    // Entête de l'e-mail
+    $entete = "From: $nom $prenom <$email>\r\n";
+    $entete .= "Reply-To: $prenom <$email>\r\n";
+
+    // Envoi de l'e-mail
+    if (mail($destinataire, 'Nouveau message depuis le formulaire de contact', $message, $entete)) {
+        echo 'Votre message a bien été envoyé !';
+    } else {
+        echo 'Erreur lors de l\'envoi de l\'e-mail.';
+    }
+}
+?>
